1fd66c74576110aefb7f158d4ac44681328f289d
Daniel <d.akipskiy@gmail.com>
2023-09-29 16:27:54 +0800

    Implement Master Client

--- Code/TheOtherSideMP/Game/Modules/MasterSystem/MasterClient.cpp
+++ Code/TheOtherSideMP/Game/Modules/MasterSystem/MasterClient.cpp
@@ -1,1 +1,239 @@
+#include "StdAfx.h"
+
+//#include "HUD/HUD.h"
+//#include "HUD/HUDScopes.h"
+//#include "HUD/HUDCrosshair.h"
+//#include "HUD/HUDSilhouettes.h"
+
 #include "TheOtherSideMP/Actors/Player/TOSPlayer.h"
+#include "TheOtherSideMP/Game/Modules/MasterSystem/RMISender.h"
+#include "TheOtherSideMP/Game/TOSGame.h"
+
+//#include "PlayerInput.h"
+
+#include "MasterClient.h"
+
+CTOSMasterClient::CTOSMasterClient(CTOSPlayer* _player) :
+	m_pLocalDude(_player)
+{
+	if (gEnv->bClient)
+	{
+		//const char* clientChannelName = g_pGame->GetIGameFramework()->GetClientChannel()->GetName();
+		//const char* netChName = _player->GetEntity()->;
+
+		//CryLogAlways(" ");
+		//CryLogAlways("[C++][CallConstructor][CTOSMasterClient] Player: %s, ClientChName: %s",
+		//	_player->GetEntity()->GetName(), clientChannelName);
+		//[C++][CallConstructor][CTOSMasterClient] Player: Akeeper, ClientChName: lmlicenses.wip4.adobe.com:64100
+
+		//TODO: Invoke RMI to Server from Client
+		//g_pTOSGame->GetModuleMasterSystem()->MasterAdd(m_pLocalDude->GetEntity());
+
+		//InvokeRMI(ClTempRadarEntity(), params, eRMI_ToClientChannel, GetChannelId(*it));
+
+		auto pSender = g_pTOSGame->GetModuleMasterSystem()->GetRMISender();
+		assert(pSender);
+
+		MasterAddingParams params;
+		params.entityId = m_pLocalDude->GetEntityId();
+
+		pSender->RMISend(CTOSMasterRMISender::SvRequestMasterAdd(), params, eRMI_ToServer);
+	}
+}
+
+CTOSMasterClient::~CTOSMasterClient()
+{
+	//TODO: Invoke RMI to Server from Client
+	//g_pTOSGame->GetModuleMasterSystem()->MasterRemove(m_pLocalDude->GetEntity());
+
+	// delete this;
+}
+
+//void CMasterClient::InitDudeMaster(bool toStart)
+//{
+//    if (m_pLocalDude)
+//    {
+//        //CPlayer::TAlienInterferenceParams lastInterferenceParams;
+//
+//        CNanoSuit* pSuit = m_pLocalDude->GetNanoSuit();
+//
+//        //Fix the non-resetted Dude player movement after controlling the actor;
+//        if (m_pLocalDude->GetPlayerInput())
+//            m_pLocalDude->GetPlayerInput()->Reset();
+//
+//        //before link to the new actor
+//        if (toStart)
+//        {
+//            m_lastDudePosition = m_pLocalDude->GetEntity()->GetWorldPos();
+//            m_lastDudeRotation = m_pLocalDude->GetViewRotation();
+//
+//            if (gEnv->bServer)
+//            {
+//                IAIObject* pAI = m_pLocalDude->GetEntity()->GetAI();
+//                if (pAI)
+//                {
+//                    if (pAI->CastToIAIActor())
+//                    {
+//                        m_lastDudeSpecies = pAI->CastToIAIActor()->GetParameters().m_nSpecies;
+//                        //CryLogAlways("SControlClient::PrepareDude -->> save player species");
+//                    }
+//
+//                    pAI->Event(AIEVENT_DISABLE, nullptr);
+//                }
+//            }
+//
+//            //I dont know, it is work?
+//            m_pLocalDude->ResetScreenFX();
+//
+//            if (pSuit)
+//            {
+//                m_lastDudeSuitEnergy = pSuit->GetSuitEnergy();
+//                m_lastDudeNanoMode = pSuit->GetMode();
+//                pSuit->SetMode(NANOMODE_DEFENSE);
+//                pSuit->SetModeDefect(NANOMODE_CLOAK, true);
+//                pSuit->SetModeDefect(NANOMODE_SPEED, true);
+//                pSuit->SetModeDefect(NANOMODE_STRENGTH, true);
+//            }
+//
+//            // Turning off a Alien screen interference effects
+//            //lastInterferenceParams = m_pLocalDude->m_interferenceParams;
+//            m_pLocalDude->ClearInterference();
+//
+//            if (g_pGame->GetHUD())
+//            {
+//                //LoadHUD(true); deprecated
+//                //m_pAbilitiesSystem->InitHUD(true);			
+//                //m_pAbilitiesSystem->ShowHUD(true);
+//                //m_pAbilitiesSystem->UpdateHUD();
+//                //m_pAbilitiesSystem->ReloadHUD();
+//
+//                //SetAmmoHealthHUD();
+//
+//                //g_pGame->GetHUD()->UpdateHealth(m_pControlledActor);
+//                //g_pGame->GetHUD()->m_animPlayerStats.Reload(true);
+//
+//                CHUDCrosshair* pCrosshair = g_pGame->GetHUD()->GetCrosshair();
+//                if (pCrosshair)
+//                {
+//                    pCrosshair->SetOpacity(1.0f);
+//                    pCrosshair->SetCrosshair(g_pGameCVars->hud_crosshair);
+//                }
+//            }
+//
+//            if (!gEnv->bEditor)
+//            {
+//                //TODO: fix "Pure function error" 	
+//
+//                /*CGameRules* pGR = g_pGame->GetGameRules();
+//                if (pGR && !m_isHitListener)
+//                {
+//                    m_isHitListener = true;
+//                    pGR->AddHitListener(this);
+//                }*/
+//            }
+//        }
+//        else
+//        {
+//            //after unlink
+//
+//            //The Player after unlink
+//            {
+//                SActorParams* pParams = m_pLocalDude->GetActorParams();
+//
+//                if (m_pLocalDude->GetHealth() > 0)
+//                {
+//                    m_pLocalDude->GetEntity()->SetPos(m_lastDudePosition);
+//
+//                    //may be bugged, not checked at 19.12.2020 0:14
+//                    m_pLocalDude->SetViewRotation(m_lastDudeRotation);
+//                }
+//
+//
+//                m_pLocalDude->SetSlaveEntityId(0);
+//                //m_pLocalDude->m_interferenceParams = lastInterferenceParams;
+//                m_pLocalDude->InitInterference();
+//                m_pLocalDude->ResetScreenFX();
+//
+//                if (m_pLocalDude->IsThirdPerson())
+//                    m_pLocalDude->ToggleThirdPerson();
+//
+//                SAFE_HUD_FUNC(GetCrosshair()->ShowFriendCross(false))
+//                SAFE_HUD_FUNC(SetWeaponName(""))
+//
+//               // g_pControlSystem->GetSquadSystem()->AnySquadClientLeft();
+//
+//                //auto* pSquad = g_pControlSystem->GetSquadSystem()->GetSquadFromMember(m_pLocalDude, true);
+//                //if (pSquad && pSquad->GetLeader() != nullptr)
+//                //    pSquad->OnPlayerAdded();
+//
+//                //Clean the OnUseData from player .lua script
+//                IScriptTable* pTable = m_pLocalDude->GetEntity()->GetScriptTable();
+//                if (pTable)
+//                {
+//                    const ScriptAnyValue value = 0;
+//                    Script::CallMethod(pTable, "SetOnUseData", value, value);
+//                }
+//
+//                if (pSuit)
+//                {
+//                    if (m_pLocalDude->GetHealth() > 0)
+//                    {
+//                        pSuit->Reset(m_pLocalDude);
+//
+//                        pSuit->SetModeDefect(NANOMODE_CLOAK, false);
+//                        pSuit->SetModeDefect(NANOMODE_SPEED, false);
+//                        pSuit->SetModeDefect(NANOMODE_STRENGTH, false);
+//
+//                        pSuit->ActivateMode(NANOMODE_CLOAK, true);
+//                        pSuit->ActivateMode(NANOMODE_SPEED, true);
+//                        pSuit->ActivateMode(NANOMODE_STRENGTH, true);
+//
+//                        pSuit->SetSuitEnergy(m_lastDudeSuitEnergy);
+//                        pSuit->SetMode(m_lastDudeNanoMode);
+//                    }
+//
+//                    if (g_pGame->GetHUD())
+//                    {
+//                        SetAmmoHealthHUD(m_pLocalDude, "Libs/UI/HUD_AmmoHealthEnergySuit.gfx");
+//                        SetInventoryHUD(m_pLocalDude, "Libs/UI/HUD_WeaponSelection.gfx");
+//                        m_animScoutFlyInterface.Unload();
+//
+//                        switch (pSuit->GetMode())
+//                        {
+//                        case NANOMODE_DEFENSE:
+//                            g_pGame->GetHUD()->m_animPlayerStats.Invoke("setMode", "Armor");
+//                            break;
+//                        case NANOMODE_SPEED:
+//                            g_pGame->GetHUD()->m_animPlayerStats.Invoke("setMode", "Speed");
+//                            break;
+//                        case NANOMODE_STRENGTH:
+//                            g_pGame->GetHUD()->m_animPlayerStats.Invoke("setMode", "Strength");
+//                            break;
+//                        case NANOMODE_CLOAK:
+//                            g_pGame->GetHUD()->m_animPlayerStats.Invoke("setMode", "Cloak");
+//                            break;
+//                        case NANOMODE_INVULNERABILITY:
+//                        case NANOMODE_DEFENSE_HIT_REACTION:
+//                        case NANOMODE_LAST:
+//                            break;
+//                        }
+//                    }
+//
+//                    pParams->vLimitRangeH = 0;
+//                    pParams->vLimitRangeV = pParams->vLimitRangeVDown = pParams->vLimitRangeVUp = 0;
+//                }
+//
+//                if (gEnv->bServer)
+//                {
+//                    IAIObject* pAI = m_pLocalDude->GetEntity()->GetAI();
+//                    if (pAI)
+//                    {
+//                        pAI->Event(AIEVENT_ENABLE, nullptr);
+//                        SetDudeSpecies(m_lastDudeSpecies);
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
--- Code/TheOtherSideMP/Game/Modules/MasterSystem/MasterClient.h
+++ Code/TheOtherSideMP/Game/Modules/MasterSystem/MasterClient.h
@@ -5,3 +5,246 @@
 #include <IGameFramework.h>
 #include <IGameRulesSystem.h>
 #include <IVehicleSystem.h>
+
+#include "TheOtherSideMP/Actors/player/TOSPlayer.h"
+#include "TheOtherSideMP/Game/Modules/ITOSGameModule.h"
+
+
+class CTOSAIActionTracker;
+class CTOSMasterClient;
+class CSquadSystem;
+class CGameFlashAnimation;
+class CConquerorSystem;
+class CTOSAbilitiesSystem;
+class CVehicleMovementBase;
+
+struct IHardwareMouseEventListener;
+struct IHitListener;
+
+
+ /**
+ * TOS MasterClient
+ * Класс описывает то, как будет происходить взаимодействие между Мастером и Рабом.
+ * В частности перемещение, управление, угол обзора и т.д
+ * Автоудаление: отсутствует.
+ */
+class CTOSMasterClient
+{	//Methods
+public:
+	friend class CGameRules;
+	//friend class CControlSystem;
+	//friend class CNetControlClient;
+	//friend class CSquadSystem;
+	//friend class CConquerorSystem;
+	friend class CPlayerInput;
+
+	friend class CTOSAbilitiesSystem;
+	friend class CAbilityOwner;
+
+	//friend class CSquad;
+	//friend class CMember;
+
+
+	explicit CTOSMasterClient(CTOSPlayer* _player);
+	~CTOSMasterClient();
+
+	//IHitListener
+	//void OnHit(const HitInfo&) override;
+	//void OnExplosion(const ExplosionInfo&) override;
+	//void OnServerExplosion(const ExplosionInfo&) override;
+	//~IHitListener
+
+	//void		ApplyMovement(const Vec3& delta);
+	//void		FullSerialize(TSerialize ser);
+
+	//void		Update();
+	//void		UpdateView(SViewParams& viewParams);
+	//void		UpdateUsability(const IEntity* pTarget) const;
+	//void		UpdateCrosshair();
+	//void		UpdateScout();
+	//void		UpdateTrooper();
+	//void		UpdateHunter() const;
+
+	//static void	SetDudeSpecies(const int species);
+	//void		SetSlaveActor(IActor* act);
+
+	//void		ToggleDudeHide(const bool toggle);
+	//void		ToggleDudeBeam(const bool toggle);
+	////Tutorial mode - its mode when the controlled actor will be revive after he die and squad system will not be change controlled actor.
+	//void		ToggleTutorialMode(const bool mode);
+
+	//IActor* GetSlaveActor() const { return m_pSlaveActor; };
+	//IEntity* GetSlaveEntity() const
+	//{
+	//	if (m_pSlaveActor) { return m_pSlaveActor->GetEntity(); } return nullptr;
+	//};
+
+	////Get the class name from the current controlled actor
+	//string		GetActorClassName() const { if (m_pSlaveActor) return m_pSlaveActor->GetEntity()->GetClass()->GetName(); else return ""; };
+
+	////Get the current item class name from the current controlled actor
+	//string		GetItemClassName() const { if (m_pSlaveActor) { if (m_pSlaveActor->GetInventory()) { const EntityId pItemId = m_pSlaveActor->GetInventory()->GetCurrentItem(); if (pItemId) { const IItem* pItem = g_pGame->GetIGameFramework()->GetIItemSystem()->GetItem(pItemId); if (pItem) { return pItem->GetEntity()->GetClass()->GetName(); } else return ""; } else return ""; } else return ""; } else return ""; };
+
+	//Tutorial mode - its mode when controlled actor will be revive when he die and squad system will not be change controlled actor when he die.
+	//bool		GetTutorialMode() const;
+
+	////Get the pointer to the material of the current controlled actor
+	//IMaterial* GetMaterial() const;
+
+	////Get the current crosshair's position vector in the world space
+	//Vec3		GetCrosshairPos() const { return m_crosshairPos; };
+	//Vec3		GetMeleeHitPos() const { return m_meleeRayhit.pt; };
+	//float		GetMeleeHits() const { return m_meleeHits; };
+
+	//static CWeapon* GetCurrentWeapon(const CActor* pActor);
+
+	//std::map<ActionId, int> GetActions() { return m_actionsMap; };
+
+	////Get structures with variables of the system and controlled actor
+	//SGenericControlParams& GetGenericParams() { return m_generic; };
+	//SScoutControlParams& GetScoutParams() { return m_scout; };
+	//SAlienControlParams& GetNakedParams() { return m_nakedAlien; };
+	//SHunterControlParams& GetHunterParams() { return m_hunter; };
+	//STrooperControlParams& GetTrooperParams() { return m_trooper; };
+
+	////These functions allows us to find out the type of controlled actor
+	//bool		IsNakedAlien() const { return GetActorClassName() == "Alien"; }
+	//bool		IsTrooper() const { return GetActorClassName() == "Trooper"; }
+	//bool		IsHunter() const { return GetActorClassName() == "Hunter"; }
+	//bool		IsScout() const { return GetActorClassName() == "Scout"; }
+
+private:
+
+	//Events
+	//void		OnChangedSpectatorMode(const IActor* pActor, uint8 mode, EntityId targetId, bool resetAll);
+	//void		OnClientHit(const HitInfo&) const;
+	//void		OnActorDeath(IActor* pActor);
+	//void		OnAction(const ActionId& action, int activationMode, float value);//Called  from PlayerInput.cpp
+	////~Events
+
+	//void		SubEnergy(const float subtractValue) const;
+
+	//bool		OnActionViewLock(EntityId entityId, const ActionId& actionId, int activationMode, float value);
+	//bool		OnActionMoveForward(EntityId entityId, const ActionId& actionId, int activationMode, float value);
+	//bool		OnActionMoveBack(EntityId entityId, const ActionId& actionId, int activationMode, float value);
+	//bool		OnActionMoveLeft(EntityId entityId, const ActionId& actionId, int activationMode, float value);
+	//bool		OnActionMoveRight(EntityId entityId, const ActionId& actionId, int activationMode, float value);
+	//void		OnActionCrouch(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionJump(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionSprint(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionBinoculars(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionAttack(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionAim(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionLeanLeft(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionLeanRight(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionNextItem(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionPrevItem(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionSpeedUp(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionSpeedDown(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionZoomIn(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionZoomOut(string classname, const ActionId& action, int activationMode, float value);
+	//void		OnActionUse(string classname, const ActionId& action, int activationMode, float value);
+
+	////Storing the current time in the variable
+	////See also CheckPassTime();
+	//void		StoreCurrTime();
+
+	////Checks how much time has passed from last time storing
+	////See also StoreCurrTime();
+	//bool		CheckPassTime(float passedSec) const;
+
+	////Resetting the structures with stored variables of the system and controlled actor
+	//void		ResetParams();
+
+	////Resetting the current controlled actor to the normal state i.e before having owner
+	//void		Reset(bool toEditor);
+
+	//Preparing a dude actor (Nomad) for to be in a controlling state
+	//void InitDudeMaster(bool toStart);
+
+	////Un/Registering the AI of controlled actor to according with the AI type of the dude player
+	//void		SetActorAI(const IActor* pActor, bool bToPlayerAI) const;
+
+	////Preparing a new actor to enter in the controlled state
+	//bool		DoNextActor(CActor* pNextActor);
+
+	////Preparing a old actor to enter in the uncontrolled state
+	//bool		DoPrevActor(CActor* pActor) const;
+
+	////Get the entity which being in the ray with length == 3 from the weapon's position to crosshair point position
+	//IEntity* GetMeleeTarget() const;
+
+	////Get the current entity under the crosshair
+	//IEntity* GetLastCrosshairEntity() const;
+
+	////Get the current entity under the crosshair
+	//IEntity* GetCrosshairEntity() const;
+
+	////Get the current entity under the crosshair
+	//EntityId	GetCrosshairEntityId() const;
+
+	//EntityId	GetScoutAutoAimTargetId() const;
+
+	////Get the entity which being in the ray with length == 5000 from weapon's position to crosshair point position
+	//IEntity* GetFireTarget() const;
+
+	//static void		SetInventoryHUD(IActor* pActor, const char* file);
+	//static void		SetAmmoHealthHUD(IActor* pActor, const char* file);
+	//IAIObject* GetCrosshairTargetAI() const;
+	//void		SendPipeToAIGroup(const char* name, int groupId, bool useInComm = 0, Vec3 refPoint = Vec3(0, 0, 0)) const;
+	//bool		NetPlayAnimAction(const char* action, bool looping) const;
+	//bool		NetSpawnParticleEffect(const char* effectName) const;
+	//void		GetMemoryStatistics(ICrySizer*) const;
+
+private:
+
+	//CGameFlashAnimation m_animScoutFlyInterface;
+
+	//std::map<ActionId, int>				  m_actionsMap;
+	//static TActionHandler<CTOSMasterClient> s_actionHandler;
+
+	//bool		m_isDebugLog;
+	//uint8		m_lastSpectatorMode;
+	//EntityId	m_mpLastControlledId;
+	//Vec3		m_finalFireTargetPos;
+	//ENanoMode	m_lastDudeNanoMode;
+	//Quat		m_lastDudeRotation;
+	//Vec3		m_lastDudePosition;
+	//int			m_lastDudeSpecies;
+	//float		m_lastDudeSuitEnergy;
+	//float		m_storedTime;
+	//float		m_meleeHits;
+	//float		m_currentFov;
+	//Vec3		m_dudeLastPos;
+	//Vec3		m_camViewDir;
+	//Vec3		m_camPos;
+	//Vec3		m_camViewCoords;
+	//Vec3		m_crosshairPos;
+	////SAlienControlParams		m_nakedAlien;
+	////STrooperControlParams	m_trooper;
+	////SScoutControlParams		m_scout;
+	////SHunterControlParams	m_hunter;
+	////SGenericControlParams	m_generic;
+	//bool		m_mustBeamDude;
+	//bool		m_mustHideDude;
+	//bool		m_canProceedActions;
+	//bool		m_isHitListener;
+
+	//IActor*		m_pSlaveActor;
+	//EntityId	m_fireTargetId;
+	//EntityId	m_meleeTargetId;
+	//EntityId	m_crosshairTargetId;
+	//EntityId	m_lastCrosshairTargetId;
+	//EntityId	m_scoutAimTargetId;
+	//ray_hit		m_meleeRayhit;
+	//ray_hit		m_crosshairRayHit;
+	//CMovementRequest m_lookRequest;// Needed only for look and aim processing
+	//CTOSAbilitiesSystem* m_pAbilitiesSystem;
+
+	CTOSPlayer* m_pLocalDude;
+
+public:
+	static constexpr int INPUT_ASPECT = eEA_GameClientDynamic;
+	static constexpr int ALIVE_ASPECT = eEA_GameServerDynamic;
+	static constexpr int OWNER_ASPECT = eEA_GameServerStatic;
+};
