f9eca38c8d816e3d84335003d06ad97664d04d19
Daniel <d.akipskiy@gmail.com>
2023-09-29 22:31:28 +0800

    Improve TOSPlayer class debug log

--- Code/TheOtherSideMP/Actors/Player/TOSPlayer.cpp
+++ Code/TheOtherSideMP/Actors/Player/TOSPlayer.cpp
@@ -1,6 +1,8 @@
 #include "StdAfx.h"
 #include "TOSPlayer.h"
 #include "TheOtherSideMP\Game\Modules\MasterSystem\MasterClient.h"
+#include "TheOtherSideMP\Game\Modules\MasterSystem\RMISender.h"
+#include "TheOtherSideMP\Game\TOSGame.h"
 
 CTOSPlayer::CTOSPlayer():
 	m_pMasterClient(0)
@@ -35,16 +37,16 @@ void CTOSPlayer::InitClient(int channelId)
 {
 	CPlayer::InitClient(channelId);
 
-	if (gEnv->bServer)
-	{
-		CryLogAlways(" ");
-		CryLogAlways("[C++][SERVER][FUNC CALL][CPlayer::InitClient] channelId: %i, ThisPlayer: %s", channelId, GetEntity()->GetName());
-	}
-	else if(gEnv->bClient)
-	{
-		CryLogAlways(" ");
-		CryLogAlways("[C++][CLIENT][FUNC CALL][CPlayer::InitClient] channelId: %i, ThisPlayer: %s", channelId, GetEntity()->GetName());
-	}
+	//if (gEnv->bServer)
+	//{
+	//	CryLogAlways(" ");
+	//	CryLogAlways("[C++][SERVER][FUNC CALL][CPlayer::InitClient] channelId: %i, ThisPlayer: %s", channelId, GetEntity()->GetName());
+	//}
+	//else if(gEnv->bClient)
+	//{
+	//	CryLogAlways(" ");
+	//	CryLogAlways("[C++][CLIENT][FUNC CALL][CPlayer::InitClient] channelId: %i, ThisPlayer: %s", channelId, GetEntity()->GetName());
+	//}
 
 	//Case 3
 	//if (!m_pMasterClient)
@@ -60,17 +62,17 @@ void CTOSPlayer::InitLocalPlayer()
 	if (gEnv->bServer)
 	{
 		CryLogAlways(" ");
-		CryLogAlways("[C++][SERVER][FUNC CALL][CPlayer::InitLocalPlayer] Player: %s", GetEntity()->GetName());
+		CryLogAlways("[C++][SERVER][FUNC CALL][CTOSPlayer::InitLocalPlayer] Player: %s", GetEntity()->GetName());
 	}
 	else if (this->GetEntityId() == g_pGame->GetIGameFramework()->GetClientActorId())
 	{
 		CryLogAlways(" ");
-		CryLogAlways("[C++][LOCAL][FUNC CALL][CPlayer::InitLocalPlayer] Player: %s", GetEntity()->GetName());
+		CryLogAlways("[C++][LOCAL][FUNC CALL][CTOSPlayer::InitLocalPlayer] Player: %s", GetEntity()->GetName());
 	}
 	else if (gEnv->bClient)
 	{
 		CryLogAlways(" ");
-		CryLogAlways("[C++][CLIENT][FUNC CALL][CPlayer::InitLocalPlayer] Player: %s", GetEntity()->GetName());
+		CryLogAlways("[C++][CLIENT][FUNC CALL][CTOSPlayer::InitLocalPlayer] Player: %s", GetEntity()->GetName());
 	}
 
 	//Case 4 perfect - Master Client was created only on local machine
@@ -95,8 +97,35 @@ bool CTOSPlayer::NetSerialize(TSerialize ser, EEntityAspects aspect, uint8 profi
 
 void CTOSPlayer::Release()
 {
+	//if (gEnv->bServer)
+	//	CryLogAlways("[C++][SERVER][FUNC CALL][CTOSPlayer::Release] PlayerId: %i", GetEntityId());
+	//else if (gEnv->bClient)
+	//	CryLogAlways("[C++][CLIENT][FUNC CALL][CTOSPlayer::Release] PlayerId: %i", GetEntityId());
+
+	//Case 2 not work
+	/*if (gEnv->bClient)
+	{
+		auto pSender = g_pTOSGame->GetMasterModule()->GetRMISender();
+		assert(pSender);
+
+		MasterAddingParams params;
+		params.entityId = 3323;
+
+		pSender->RMISend(CTOSMasterRMISender::SvRequestMasterAdd(), params, eRMI_ToServer);
+	}*/
+
+	//Case 3 not work
+	//auto pSender = g_pTOSGame->GetMasterModule()->GetRMISender();
+	//assert(pSender);
+	//
+	//PintestParams params;
+	//params.commentary = "[CTOSPlayer::Release]";
+
+	//if (gEnv->bClient)
+	//	pSender->RMISend(CTOSMasterRMISender::SvRequestPintest(), params, eRMI_ToServer);
+
 	SAFE_DELETE(m_pMasterClient);
-	CActor::Release();
+	CPlayer::Release();
 }
 
 CTOSMasterClient* CTOSPlayer::GetMasterClient()
