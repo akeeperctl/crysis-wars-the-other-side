5b610fed3aeed5b2a1cd26f1cdd045356c3474bb
Daniel <d.akipskiy@gmail.com>
2023-10-01 10:44:26 +0800

    Implement a module to revive entites that were deleted when executing the sv_restart command.

---
+++ Code/TheOtherSideMP/Game/Modules/RestartResurrection/RestartResurrectionModule.cpp
@@ -0,0 +1,2 @@
+#include "StdAfx.h"
+#include "RestartResurrectionModule.h"
---
+++ Code/TheOtherSideMP/Game/Modules/RestartResurrection/RestartResurrectionModule.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class CTOSRestartResurrectionModule
+{
+public:
+	CTOSRestartResurrectionModule();
+	~CTOSRestartResurrectionModule();
+
+private:
+
+};
