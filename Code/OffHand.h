26bb5b0c58ce0c5ee1a788a071a78d93547d8e5d
Daniel <d.akipskiy@gmail.com>
2023-10-21 00:43:01 +0800

    Revert "[Weapon] bring code to a unified style"
    This reverts commit 436132ef91b5367353c7b2c1419b87fe62a2db7d.

--- Code/OffHand.h
+++ Code/OffHand.h
@@ -31,144 +31,132 @@ History:
 
 enum EOffHandActions
 {
-	eOHA_NO_ACTION               = 0,
-	eOHA_SWITCH_GRENADE          = 1,
-	eOHA_THROW_GRENADE           = 2,
-	eOHA_USE                     = 3,
-	eOHA_PICK_ITEM               = 4,
-	eOHA_PICK_OBJECT             = 5,
-	eOHA_THROW_OBJECT            = 6,
-	eOHA_GRAB_NPC                = 7,
-	eOHA_THROW_NPC               = 8,
-	eOHA_RESET                   = 9,
+	eOHA_NO_ACTION				= 0,
+	eOHA_SWITCH_GRENADE		= 1,
+	eOHA_THROW_GRENADE		= 2,
+	eOHA_USE							= 3,
+	eOHA_PICK_ITEM				= 4,
+	eOHA_PICK_OBJECT			= 5,
+	eOHA_THROW_OBJECT			= 6,
+	eOHA_GRAB_NPC					= 7,
+	eOHA_THROW_NPC				= 8,
+	eOHA_RESET						= 9,
 	eOHA_FINISH_AI_THROW_GRENADE = 10,
-	eOHA_MELEE_ATTACK            = 11,
-	eOHA_FINISH_MELEE            = 12,
-	eOHA_REINIT_WEAPON           = 13,
+	eOHA_MELEE_ATTACK			= 11,
+	eOHA_FINISH_MELEE			= 12,
+	eOHA_REINIT_WEAPON		= 13,
 };
 
 enum EOffHandStates
 {
-	eOHS_INIT_STATE        = 0x00000001,
-	eOHS_SWITCHING_GRENADE = 0x00000002,
-	eOHS_HOLDING_GRENADE   = 0x00000004,
-	eOHS_THROWING_GRENADE  = 0x00000008,
-	eOHS_PICKING           = 0x00000010,
-	eOHS_PICKING_ITEM      = 0x00000020,
-	eOHS_PICKING_ITEM2     = 0x00000040,
-	eOHS_HOLDING_OBJECT    = 0x00000080,
-	eOHS_THROWING_OBJECT   = 0x00000100,
-	eOHS_GRABBING_NPC      = 0x00000200,
-	eOHS_HOLDING_NPC       = 0x00000400,
-	eOHS_THROWING_NPC      = 0x00000800,
-	eOHS_TRANSITIONING     = 0x00001000,
-	eOHS_MELEE             = 0x00002000
+	eOHS_INIT_STATE					= 0x00000001,
+	eOHS_SWITCHING_GRENADE  = 0x00000002,
+	eOHS_HOLDING_GRENADE		= 0x00000004,
+	eOHS_THROWING_GRENADE   = 0x00000008,
+	eOHS_PICKING						= 0x00000010,
+	eOHS_PICKING_ITEM				= 0x00000020,
+	eOHS_PICKING_ITEM2			= 0x00000040,
+	eOHS_HOLDING_OBJECT			= 0x00000080,
+	eOHS_THROWING_OBJECT		= 0x00000100,
+	eOHS_GRABBING_NPC				= 0x00000200,
+	eOHS_HOLDING_NPC				= 0x00000400,
+	eOHS_THROWING_NPC				= 0x00000800,
+	eOHS_TRANSITIONING			= 0x00001000,
+	eOHS_MELEE							= 0x00002000
 };
 
 enum EOffHandSounds
 {
-	eOHSound_Choking_Trooper = 0,
-	eOHSound_Choking_Human   = 1,
-	eOHSound_Kill_Human      = 2,
-	eOHSound_LastSound       = 3
+	eOHSound_Choking_Trooper	= 0,
+	eOHSound_Choking_Human		= 1,
+	eOHSound_Kill_Human				= 2, 
+	eOHSound_LastSound				= 3
 };
+
 
 class COffHand : public CWeapon
 {
+
 	typedef struct SGrabType
 	{
-		ItemString helper;
-		ItemString pickup;
-		ItemString idle;
-		ItemString throwFM;
-		bool       twoHanded;
+		ItemString	helper;
+		ItemString	pickup;
+		ItemString	idle;
+		ItemString	throwFM;
+		bool		twoHanded;
 	};
 
-	typedef std::vector<SGrabType> TGrabTypes;
+	typedef std::vector<SGrabType>				TGrabTypes;
 
 public:
+
 	COffHand();
-	~COffHand() override;
+	virtual ~COffHand();
 
-	void Update(SEntityUpdateContext& ctx, int slot) override;
-	void PostUpdate(float frameTime) override;
-	void PostInit(IGameObject* pGameObject) override;
-	void Reset() override;
+	virtual void Update(SEntityUpdateContext &ctx, int slot);
+	virtual void PostUpdate(float frameTime);
+	virtual void PostInit(IGameObject * pGameObject );
+	virtual void Reset();
 
-	void OnAction(EntityId actorId, const ActionId& actionId, int activationMode, float value) override;
+	virtual void OnAction(EntityId actorId, const ActionId& actionId, int activationMode, float value);
 
-	bool CanSelect() const override;
-	void Select(bool select) override;
-	void FullSerialize(TSerialize ser) override;
-	bool NetSerialize(TSerialize ser, EEntityAspects aspect, uint8 profile, int flags) override;
-	void PostSerialize() override;
+	virtual bool CanSelect() const;
+	virtual void Select(bool select);
+	virtual void FullSerialize( TSerialize ser );
+	virtual bool NetSerialize( TSerialize ser, EEntityAspects aspect, uint8 profile, int flags );
+	virtual void PostSerialize();
 
-	void MeleeAttack() override;
+	virtual void MeleeAttack();
 
-	void PostFilterView(struct SViewParams& viewParams) override;
+	virtual void PostFilterView(struct SViewParams &viewParams);
 
 	//Only needed because is used in CFists
-	void EnterWater(bool enter) override {}
+	virtual void EnterWater(bool enter) {}
 
-	void UpdateFPView(float frameTime) override;
+	virtual void UpdateFPView(float frameTime);
 
 	//AIGrenades (for AI)
-	void PerformThrow(float speedScale) override;
+	virtual void PerformThrow(float speedScale);
 
 	//Memory Statistics
-	void GetMemoryStatistics(ICrySizer* s) override
-	{
-		s->Add(*this);
-		CWeapon::GetMemoryStatistics(s);
-	}
+	virtual void GetMemoryStatistics(ICrySizer * s) { s->Add(*this); CWeapon::GetMemoryStatistics(s); }
 
-	void      SetOffHandState(EOffHandStates eOHS);
-	ILINE int GetOffHandState() const
-	{
-		return m_currentState;
-	}
-
-	void FinishAction(EOffHandActions eOHA);
-	void Freeze(bool freeze) override;
+	void  SetOffHandState(EOffHandStates eOHS);
+	ILINE int  GetOffHandState() { return m_currentState; } 
+	void  FinishAction(EOffHandActions eOHA);
+	virtual void Freeze(bool freeze);
 
 	bool IsHoldingEntity();
 
 	void OnBeginCutScene();
 	void OnEndCutScene();
 
-	void GetAvailableGrenades(std::vector<string>& grenades);
+	void GetAvailableGrenades(std::vector<string> &grenades);
 
-	ILINE void SetResetTimer(float t)
-	{
-		m_resetTimer = t;
-	}
+	ILINE void SetResetTimer(float t) { m_resetTimer = t;}
+	ILINE int32 GetGrabType() const { return m_grabType; }
 
-	ILINE int32 GetGrabType() const
-	{
-		return m_grabType;
-	}
+	float GetObjectMassScale();  //Scale for mouse sensitivity
 
-	float GetObjectMassScale(); //Scale for mouse sensitivity
+	virtual bool ReadItemParams(const IItemParamsNode *root);
 
-	bool ReadItemParams(const IItemParamsNode* root) override;
-
-	void SelectGrabType(IEntity* pEntity);
+	void	SelectGrabType(IEntity* pEntity);
 
 	bool EvaluateStateTransition(int requestedAction, int activationMode, int inputMethod);
-	bool PreExecuteAction(int requestedAction, int activationMode, bool forceSelect = false);
+	bool PreExecuteAction(int requestedAction, int activationMode, bool forceSelect =false);
 	void CancelAction();
 
-	void IgnoreCollisions(bool ignore, EntityId entityId = 0);
-	void DrawNear(bool drawNear, EntityId entityId = 0);
+	void IgnoreCollisions(bool ignore, EntityId entityId=0);
+	void DrawNear(bool drawNear, EntityId entityId=0);
 	bool PerformPickUp();
-	int  CanPerformPickUp(CActor* pActor, IPhysicalEntity* pPhysicalEntity = nullptr, bool getEntityInfo = false);
+	int  CanPerformPickUp(CActor *pActor, IPhysicalEntity *pPhysicalEntity = NULL, bool getEntityInfo = false);
 	int  CheckItemsInProximity(Vec3 pos, Vec3 dir, bool getEntityInfo);
 
 	void UpdateCrosshairUsabilitySP();
 	void UpdateCrosshairUsabilityMP();
 	void UpdateHeldObject();
 	void UpdateGrabbedNPCState();
-	void UpdateGrabbedNPCWorldPos(IEntity* pEntity, const struct SViewParams* viewParams);
+	void UpdateGrabbedNPCWorldPos(IEntity *pEntity, struct SViewParams *viewParams);
 
 	void StartSwitchGrenade(bool xi_switch = false, bool fakeSwitch = false);
 	void EndSwitchGrenade();
@@ -182,9 +170,9 @@ public:
 	void PickUpObject(bool isLivingEnt = false);
 	void ThrowObject(int activationMode, bool isLivingEnt = false);
 
-	void NetStartFire() override;
-	void NetStopFire() override;
-	void NetStartSecondaryFire() override {}
+	void NetStartFire();
+	void NetStopFire();
+	virtual void NetStartSecondaryFire() {}
 
 	bool GrabNPC();
 	void ThrowNPC(bool kill = true);
@@ -193,73 +181,74 @@ public:
 	void RunEffectOnGrabbedNPC(CActor* pNPC);
 	void PlaySound(EOffHandSounds sound, bool play);
 
-	EntityId GetHeldEntityId() const override;
-	bool     CheckHeldItemForConflict(EntityId target) const;
+	EntityId	GetHeldEntityId() const;
+	bool	CheckHeldItemForConflict(EntityId target) const;
 
 	void AttachGrenadeToHand(int grenade, bool fp = true, bool attach = true);
 
-	void ForcePendingActions() override {}
+	virtual void ForcePendingActions() {}
 
 private:
-	EntityId SpawnRockProjectile(IRenderNode* pRenderNode);
 
-	int    CanExchangeWeapons(IItem* pItem, IItem** pExchangeItem);
-	IItem* GetExchangeItem(IItem* pPickupItem, CPlayer* pPlayer);
+	EntityId SpawnRockProjectile(IRenderNode *pRenderNode);
 
-	void PostPostSerialize() override;
+	int		CanExchangeWeapons(IItem *pItem, IItem **pExchangeItem);
+	IItem *GetExchangeItem(IItem *pPickupItem, CPlayer *pPlayer);
+
+	void	PostPostSerialize();
 
 	//Grenade info
-	int   m_lastFireModeId;
-	float m_nextGrenadeThrow;
+	int					m_lastFireModeId;
+	float				m_nextGrenadeThrow;		
 
-	float m_lastCHUpdate;
+	float				m_lastCHUpdate;
 
 	//All what we need for grabbing
-	TGrabTypes m_grabTypes;
-	uint32     m_grabType;
-	EntityId   m_heldEntityId, m_preHeldEntityId, m_crosshairId;
-	Matrix34   m_holdOffset;
-	Vec3       m_holdScale;
-	int        m_constraintId;
-	bool       m_hasHelper;
-	int        m_grabbedNPCSpecies;
-	float      m_heldEntityMass;
+	TGrabTypes		m_grabTypes;
+	uint32				m_grabType;
+	EntityId			m_heldEntityId, m_preHeldEntityId, m_crosshairId;
+	Matrix34			m_holdOffset;
+	Vec3          m_holdScale;
+	int						m_constraintId;
+	bool					m_hasHelper;
+	int						m_grabbedNPCSpecies;
+	float					m_heldEntityMass;
+	
+	float					m_killTimeOut;
+	bool					m_killNPC;
+	bool					m_effectRunning;
+	bool					m_npcWasDead;
+	bool					m_startPickUp;
+	int						m_grabbedNPCInitialHealth;
+	bool          m_forceThrow;
 
-	float m_killTimeOut;
-	bool  m_killNPC;
-	bool  m_effectRunning;
-	bool  m_npcWasDead;
-	bool  m_startPickUp;
-	int   m_grabbedNPCInitialHealth;
-	bool  m_forceThrow;
+	tSoundID			m_sounds[eOHSound_LastSound];
 
-	tSoundID m_sounds[eOHSound_LastSound];
+	float					m_range;
+	float					m_pickingTimer;
+	float					m_resetTimer;
 
-	float m_range;
-	float m_pickingTimer;
-	float m_resetTimer;
+	int						m_usable;
 
-	int m_usable;
+	int           m_checkForConstraintDelay;
+	bool          m_bCutscenePlaying;
 
-	int  m_checkForConstraintDelay;
-	bool m_bCutscenePlaying;
-
-	float m_fGrenadeToggleTimer;
-	float m_fGrenadeThrowTimer;
+	float					m_fGrenadeToggleTimer;
+	float					m_fGrenadeThrowTimer;
 
 	//Current state and pointers to actor main item(weapon) while offHand is selected
-	int      m_currentState;
-	CItem*   m_mainHand;
-	CWeapon* m_mainHandWeapon;
-	EntityId m_prevMainHandId;
+	int						m_currentState;
+	CItem					*m_mainHand;
+	CWeapon				*m_mainHandWeapon;
+	EntityId			m_prevMainHandId;
 
-	IRenderNode* m_pRockRN;
+	IRenderNode*  m_pRockRN;
 
-	Matrix34 m_lastNPCMatrix;
-	Matrix34 m_intialBoidLocalMatrix;
+	Matrix34      m_lastNPCMatrix;
+	Matrix34			m_intialBoidLocalMatrix;
 
-	bool m_mainHandIsDualWield;
-	bool m_restoreStateAfterLoading;
+	bool					m_mainHandIsDualWield;
+	bool					m_restoreStateAfterLoading;
 
 	//Input actions
 	static TActionHandler<COffHand> s_actionHandler;
