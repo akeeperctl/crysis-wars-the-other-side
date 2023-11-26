f2985c62bc50efc358d19e27cb1d8205e6ee656a
Daniel <d.akipskiy@gmail.com>
2023-11-26 14:10:37 +0800

    [Helpers] add stl find function to

--- Code/TheOtherSideMP/Helpers/TOS_STL.h
+++ Code/TheOtherSideMP/Helpers/TOS_STL.h
@@ -11,11 +11,12 @@ namespace TOS_STL
 
 		if (itemsCount > 1)
 		{
-			const int minItemIndex = 0;
+			//const int minItemIndex = 0;
 			const int maxItemIndex = containter.size() - 1;
 
-			const float ranf = Random(minItemIndex, maxItemIndex);
-			const int ran = round(ranf);
+			//const float ranf = Random(minItemIndex, maxItemIndex);
+			//const int ran = round(ranf);
+			const float ran = Random(maxItemIndex);
 
 			for (int i = 0; i < containter.size(); i++)
 			{
@@ -28,16 +29,34 @@ namespace TOS_STL
 		}
 		else if (itemsCount == 1)
 		{
-			for (DataType itemIndex : containter)
+			for (DataType item : containter)
 			{
-				return itemIndex;
+				return item;
 			}
 		}
+
+		return containter[0];
 	}
 
 	template <class Container, typename DataType>
 	int GetIndexFromMapKey(const Container& containter, DataType keyValue)
 	{
 		return std::distance(containter.begin(), containter.find(keyValue));
+	}
+
+	//! Find element in container.
+	// @return true if item found.
+	template <class Container, class Value>
+	bool Find(const Container& container, const Value& value)
+	{
+		return std::find(container.begin(), container.end(), value) != container.end();
+	}
+
+	//! Find element in container.
+	// @return true if item found.
+	template <class Container, class Value>
+	bool Find(const Container* container, const Value& value)
+	{
+		return std::find(container->begin(), container->end(), value) != container->end();
 	}
 }
