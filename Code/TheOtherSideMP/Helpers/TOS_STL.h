f5fde777d69cf340d003f226b21c3d0aafe7737f
Daniel <d.akipskiy@gmail.com>
2024-06-23 09:48:13 +0800

    fix(STL): улучши функцию получение рандомного зн
    Улучшение функции получения рандомного значения из контейнера STL

--- Code/TheOtherSideMP/Helpers/TOS_STL.h
+++ Code/TheOtherSideMP/Helpers/TOS_STL.h
@@ -2,40 +2,65 @@
 
 #include "Cry_Math.h"
 
+#include <random>
+#include <iterator>
+#include <type_traits>
+
 namespace TOS_STL
 {
-	template <class Container, typename DataType>
-	DataType GetRandomFromSTL(const Container& containter)
+	//template <class Container, typename DataType>
+	//DataType GetRandomFromSTL(const Container& containter)
+	//{
+	//	const float itemsCount = containter.size();
+
+	//	if (itemsCount > 1)
+	//	{
+	//		//const int minItemIndex = 0;
+	//		const int maxItemIndex = containter.size() - 1;
+
+	//		//const float ranf = Random(minItemIndex, maxItemIndex);
+	//		//const int ran = round(ranf);
+	//		const float ran = Random(maxItemIndex);
+
+	//		for (int i = 0; i < containter.size(); i++)
+	//		{
+	//			if (i != ran)
+	//				continue;
+
+	//			return containter[i];
+	//		}
+
+	//	}
+	//	else if (itemsCount == 1)
+	//	{
+	//		for (DataType item : containter)
+	//		{
+	//			return item;
+	//		}
+	//	}
+
+	//	return containter[0];
+	//}
+
+	template <class Container>
+	typename std::enable_if<!std::is_void<decltype(*std::begin(std::declval<Container>()))>::value, decltype(*std::begin(std::declval<Container>()))>::type
+		GetRandomFromSTL(const Container& container) 
 	{
-		const float itemsCount = containter.size();
+		size_t itemsCount = std::distance(std::begin(container), std::end(container));
 
-		if (itemsCount > 1)
-		{
-			//const int minItemIndex = 0;
-			const int maxItemIndex = containter.size() - 1;
-
-			//const float ranf = Random(minItemIndex, maxItemIndex);
-			//const int ran = round(ranf);
-			const float ran = Random(maxItemIndex);
-
-			for (int i = 0; i < containter.size(); i++)
-			{
-				if (i != ran)
-					continue;
-
-				return containter[i];
-			}
-
-		}
-		else if (itemsCount == 1)
-		{
-			for (DataType item : containter)
-			{
-				return item;
-			}
+		if (itemsCount == 0) {
+			// Возвращаем значение по умолчанию для типа элементов контейнера.
+			return decltype(*std::begin(container))();
 		}
 
-		return containter[0];
+		static std::random_device rd;
+		static std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(0, itemsCount - 1);
+
+		auto it = std::begin(container);
+		std::advance(it, dis(gen));
+
+		return *it;
 	}
 
 	template <class Container, typename DataType>
