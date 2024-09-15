6ae1a0fc25f4df0da378ca63704866b04cc395d3
Daniel <d.akipskiy@gmail.com>
2024-09-15 22:55:21 +0800

    refactor(SmartStruct): отформатирую и добавь комменты

--- Code/TheOtherSideMP/TOSSmartStruct.h
+++ Code/TheOtherSideMP/TOSSmartStruct.h
@@ -1,14 +1,21 @@
 #pragma once
 
-/**
- * \brief Используется вместе с _smart_ptr
- */
-struct STOSSmartStruct  // NOLINT(cppcoreguidelines-special-member-functions)
+// Используется вместе с _smart_ptr
+// Пример: std::vector<_smart_ptr<STOSSmartStruct>> myVec;
+struct STOSSmartStruct
 {
-	STOSSmartStruct(): m_refs(0) {}
-	virtual ~STOSSmartStruct() {};
-	virtual void AddRef() const { ++m_refs; };
-	virtual uint GetRefCount() const { return m_refs; };
+	STOSSmartStruct() : m_refs(0)
+	{}
+	virtual ~STOSSmartStruct()
+	{};
+	virtual void AddRef() const
+	{
+		++m_refs;
+	};
+	virtual uint GetRefCount() const
+	{
+		return m_refs;
+	};
 	virtual void Release() const
 	{
 		if (--m_refs <= 0)
