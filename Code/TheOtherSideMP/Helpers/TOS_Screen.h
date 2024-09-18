ba1b1b3ecc258ab596050f5e6903507bf0cebc4d
Daniel <d.akipskiy@gmail.com>
2024-09-18 09:59:27 +0800

    feat(TOS_Screen): добавь проекцию на экран

--- Code/TheOtherSideMP/Helpers/TOS_Screen.h
+++ Code/TheOtherSideMP/Helpers/TOS_Screen.h
@@ -14,4 +14,19 @@ namespace TOS_Screen
 		screenCoords.x *= 0.01f * fWidth;
 		screenCoords.y *= 0.01f * fHeight;
 	}
+
+	inline Vec3 ProjectToScreen(const Vec3& worldCoords)
+	{
+		Vec3 screenCoords(ZERO);
+
+		gEnv->pRenderer->ProjectToScreen(worldCoords.x, worldCoords.y, worldCoords.z, &screenCoords.x, &screenCoords.y, &screenCoords.z);
+		const float fWidth = gEnv->pRenderer->GetWidth();
+		const float fHeight = gEnv->pRenderer->GetHeight();
+
+		//scale projected values to the actual screen resolution
+		screenCoords.x *= 0.01f * fWidth;
+		screenCoords.y *= 0.01f * fHeight;
+
+		return screenCoords;
+	}
 }
