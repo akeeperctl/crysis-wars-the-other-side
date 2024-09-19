470ebfe0f584264176a05233db543ab0ffac7ea8
Daniel <d.akipskiy@gmail.com>
2024-09-19 10:56:55 +0800

    feat(AI): добавь поведение TOSSHAREd

--- Game/Scripts/AI/TOS/Behaviors/TOSSHARED.lua
+++ Game/Scripts/AI/TOS/Behaviors/TOSSHARED.lua
@@ -3,169 +3,148 @@
 --   Description: Поведение для регистрации функций как сигналов из TOS_AI.h
 --------------------------
 --
+Script.ReloadScript("Scripts/Utils/TOSTable.lua")
 
 AIBehaviour.TOSSHARED = {
 	Name = "TOSSHARED",
+	-- NOPREVIOUS = 1,
+	-- Alertness = 0,
+	-- Exclusive = 1,
+	-- LeaveCoverOnStart = 1,
 
 	-- TheOtherSide
 	---------------------------------------------
-	Constructor = function (self, entity)
+	Constructor = function (self, entity, data)
 		-- called when the behaviour is selected
 		-- the extra data is from the signal that caused the behavior transition
-		System.LogAlways(string.format("[%s] TOSSHARED.Constructor", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.Constructor data %s", EntityName(entity), table.safedump(data))
 	end,
 	---------------------------------------------
-	Destructor = function (self, entity,data)
+	Destructor = function (self, entity, data)
 		-- called when the behaviour is de-selected
 		-- the extra data is from the signal that is causing the behavior transition
-	end,
-
-	ENABLE_COMBAT = function(self, entity, sender, data)
-		entity:CancelSubpipe()
-		TOS_AI.InsertSubpipe(entity, "devalue_target", AIGOALPIPE_RUN_ONCE, 0, 0)
+		LogAlways("[%s] TOSSHARED.Destructor data %s", EntityName(entity), EntityName(data))
 	end,
 	-- ~TheOtherSide
 
 	-- События из CRYENGINE 5
-	OnActionCreated = function (self, entity)
-		System.LogAlways(string.format("[%s] TOSSHARED.OnActionCreated", entity:GetName()))
+	OnActionCreated = function (self, entity, data, sender)
+		LogAlways("[%s] TOSSHARED.OnActionCreated sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
 	end,
 
-	OnActionDone = function (self, entity)
-		System.LogAlways(string.format("[%s] TOSSHARED.OnActionDone", entity:GetName()))
+	OnActionDone = function (self, entity, data, sender)
+		LogAlways("[%s] TOSSHARED.OnActionDone sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
 	end,
 
-	OnActionEnd = function (self, entity)
-		System.LogAlways(string.format("[%s] TOSSHARED.OnActionEnd", entity:GetName()))
+	OnActionEnd = function (self, entity, data, sender)
+		LogAlways("[%s] TOSSHARED.OnActionEnd sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
 	end,
 
-	OnActionStart = function (self, entity)
-		System.LogAlways(string.format("[%s] TOSSHARED.OnActionStart", entity:GetName()))
+	OnActionStart = function (self, entity, data, sender)
+		LogAlways("[%s] TOSSHARED.OnActionStart sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
 	end,
 
 
 	-- SYSTEM EVENTS			-----
 	---------------------------------------------
 	OnSelected = function( self, entity )
-		System.LogAlways(string.format("[%s] TOSSHARED.OnSelected", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnSelected", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnSpawn = function( self, entity )
 		-- called when enemy spawned or reset
-		System.LogAlways(string.format("[%s] TOSSHARED.OnSpawn", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnSpawn", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnActivate = function( self, entity )
 		-- called when enemy receives an activate event (from a trigger, for example)
-		System.LogAlways(string.format("[%s] TOSSHARED.OnActivate", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnActivate", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnNoTarget = function( self, entity )
 		-- called when the enemy stops having an attention target
-		System.LogAlways(string.format("[%s] TOSSHARED.OnNoTarget", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnNoTarget", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnPlayerSeen = function( self, entity, fDistance )
 		-- called when the enemy sees a living player
-		System.LogAlways(string.format("[%s] TOSSHARED.OnPlayerSeen, fDistance = %i", entity:GetName(), tonumber(fDistance)))
+		LogAlways("[%s] TOSSHARED.OnPlayerSeen, fDistance = %i", EntityName(entity), tonumber(fDistance))
 	end,
 	---------------------------------------------
 	OnGrenadeSeen = function( self, entity, fDistance )
 		-- called when the enemy sees a grenade
-		System.LogAlways(string.format("[%s] TOSSHARED.OnGrenadeSeen, fDistance = %i", entity:GetName(), tonumber(fDistance)))
+		LogAlways("[%s] TOSSHARED.OnGrenadeSeen, fDistance = %i", EntityName(entity), tonumber(fDistance))
 	end,
 	---------------------------------------------
 	OnEnemySeen = function( self, entity )
 		-- called when the enemy sees a foe which is not a living player
-		System.LogAlways(string.format("[%s] TOSSHARED.OnEnemySeen", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnEnemySeen", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnSomethingSeen = function( self, entity )
 		-- called when the enemy sees something that it cant identify
-		System.LogAlways(string.format("[%s] TOSSHARED.OnSomethingSeen", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnSomethingSeen", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnEnemyMemory = function( self, entity )
 		-- called when the enemy can no longer see its foe, but remembers where it saw it last
-		System.LogAlways(string.format("[%s] TOSSHARED.OnEnemyMemory", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnEnemyMemory", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnInterestingSoundHeard = function( self, entity )
 		-- called when the enemy hears an interesting sound
-		System.LogAlways(string.format("[%s] TOSSHARED.OnInterestingSoundHeard", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnInterestingSoundHeard", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnThreateningSoundHeard = function( self, entity )
 		-- called when the enemy hears a scary sound
-		System.LogAlways(string.format("[%s] TOSSHARED.OnThreateningSoundHeard", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnThreateningSoundHeard", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnReload = function( self, entity )
 		-- called when the enemy goes into automatic reload after its clip is empty
-		System.LogAlways(string.format("[%s] TOSSHARED.OnReload", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnReload", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnReloadDone = function( self, entity )
 		-- called after reloading is done
-		System.LogAlways(string.format("[%s] TOSSHARED.OnReloadDone", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnReloadDone", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnGroupMemberDied = function( self, entity )
 		-- called when a member of the group dies
-		System.LogAlways(string.format("[%s] TOSSHARED.OnGroupMemberDied", entity:GetName()))
+		LogAlways("[%s] TOSSHARED.OnGroupMemberDied", EntityName(entity))
 	end,
 	---------------------------------------------
 	OnNoHidingPlace = function( self, entity, sender )
 		-- called when no hiding place can be found with the specified parameters
-		local senderName = "NONE"
-		if sender then
-			senderName = sender:GetName()
-		end
 
-		System.LogAlways(string.format("[%s] TOSSHARED.OnNoHidingPlace, sender: %s", entity:GetName(), senderName))
+		LogAlways("[%s] TOSSHARED.OnNoHidingPlace, sender: %s", EntityName(entity), EntityName(sender))
 	end,	
 	--------------------------------------------------
 	OnNoFormationPoint = function ( self, entity, sender)
 		-- called when the enemy found no formation point
 
-		local senderName = "NONE"
-		if sender then
-			senderName = sender:GetName()
-		end
-
-		System.LogAlways(string.format("[%s] TOSSHARED.OnNoFormationPoint, sender: %s", entity:GetName(), senderName))
+		LogAlways("[%s] TOSSHARED.OnNoFormationPoint, sender: %s", EntityName(entity), EntityName(sender))
 	end,
 	---------------------------------------------
 	OnReceivingDamage = function ( self, entity, sender)
 		-- called when the enemy is damaged
 
-		local senderName = "NONE"
-		if sender then
-			senderName = sender:GetName()
-		end
-
-		System.LogAlways(string.format("[%s] TOSSHARED.OnReceivingDamage, sender: %s", entity:GetName(), senderName))
+		LogAlways("[%s] TOSSHARED.OnReceivingDamage, sender: %s", EntityName(entity), EntityName(sender))
 	end,
 	---------------------------------------------
 	OnCoverRequested = function ( self, entity, sender)
 		-- called when the enemy is damaged
-		local senderName = "NONE"
-		if sender then
-			senderName = sender:GetName()
-		end
 
-		System.LogAlways(string.format("[%s] TOSSHARED.OnCoverRequested, sender: %s", entity:GetName(), senderName))
+		LogAlways("[%s] TOSSHARED.OnCoverRequested, sender: %s", EntityName(entity), EntityName(sender))
 
 	end,
 	--------------------------------------------------
 	OnBulletRain = function ( self, entity, sender)
 		-- called when the enemy detects bullet trails around him
-		local senderName = "NONE"
-		if sender then
-			senderName = sender:GetName()
-		end
 
-		System.LogAlways(string.format("[%s] TOSSHARED.OnBulletRain, sender: %s", entity:GetName(), senderName))
+		LogAlways("[%s] TOSSHARED.OnBulletRain, sender: %s", EntityName(entity), EntityName(sender))
 
 	end,
 	--------------------------------------------------
@@ -221,11 +200,11 @@ AIBehaviour.TOSSHARED = {
 	end,
 }
 
--- System.LogAlways(string.format("TOSSHARED = %s", tostring( AIBehaviour.TOSSHARED)))
--- System.LogAlways(string.format("TOSSHARED.Constructor = %s", tostring( AIBehaviour.TOSSHARED.Constructor)))
--- System.LogAlways(string.format("TOSSHARED.Destructor = %s", tostring( AIBehaviour.TOSSHARED.Destructor)))
--- System.LogAlways(string.format("TOSSHARED.ENABLE_COMBAT = %s", tostring( AIBehaviour.TOSSHARED.ENABLE_COMBAT)))
--- System.LogAlways(string.format("TOSSHARED.OnActionCreated = %s", tostring( AIBehaviour.TOSSHARED.OnActionCreated)))
--- System.LogAlways(string.format("TOSSHARED.OnActionDone = %s", tostring( AIBehaviour.TOSSHARED.OnActionDone)))
--- System.LogAlways(string.format("TOSSHARED.OnActionEnd = %s", tostring( AIBehaviour.TOSSHARED.OnActionEnd)))
--- System.LogAlways(string.format("TOSSHARED.OnActionStart = %s", tostring( AIBehaviour.TOSSHARED.OnActionStart)))
+-- LogAlways("TOSSHARED = %s", tostring( AIBehaviour.TOSSHARED))
+-- LogAlways("TOSSHARED.Constructor = %s", tostring( AIBehaviour.TOSSHARED.Constructor))
+-- LogAlways("TOSSHARED.Destructor = %s", tostring( AIBehaviour.TOSSHARED.Destructor))
+-- LogAlways("TOSSHARED.ENABLE_COMBAT = %s", tostring( AIBehaviour.TOSSHARED.ENABLE_COMBAT))
+-- LogAlways("TOSSHARED.OnActionCreated = %s", tostring( AIBehaviour.TOSSHARED.OnActionCreated))
+-- LogAlways("TOSSHARED.OnActionDone = %s", tostring( AIBehaviour.TOSSHARED.OnActionDone))
+-- LogAlways("TOSSHARED.OnActionEnd = %s", tostring( AIBehaviour.TOSSHARED.OnActionEnd))
+-- LogAlways("TOSSHARED.OnActionStart = %s", tostring( AIBehaviour.TOSSHARED.OnActionStart))
