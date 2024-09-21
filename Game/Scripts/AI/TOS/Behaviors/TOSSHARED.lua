c7f43b305ada857b0cb6124a63810bc7dd11879d
Daniel <d.akipskiy@gmail.com>
2024-09-21 10:17:30 +0800

    feat(AI): добавь еще событий в TOSSHARED

--- Game/Scripts/AI/TOS/Behaviors/TOSSHARED.lua
+++ Game/Scripts/AI/TOS/Behaviors/TOSSHARED.lua
@@ -3,11 +3,15 @@
  --Copyright (C), AlienKeeper, 2024.
 --*************************************************************************
 
+-- Файл используется для обучения и понимания работы поведений в CE2.
+-- Не все функции сигналов вызываются, поэтому здесь всюду логи.
+
+Script.ReloadScript("Scripts/Utils/TOSCommon.lua")
 Script.ReloadScript("Scripts/Utils/TOSTable.lua")
 
 AIBehaviour.TOSSHARED = {
 	Name = "TOSSHARED",
-	-- NOPREVIOUS = 1,
+	NOPREVIOUS = 1, -- Если 1, то не изменяет предыдущее поведение, записанное в "PREVIOUS"
 	-- Alertness = 0,
 	-- Exclusive = 1,
 	-- LeaveCoverOnStart = 1,
@@ -27,23 +31,176 @@ AIBehaviour.TOSSHARED = {
 	end,
 	-- ~TheOtherSide
 
-	-- События из CRYENGINE 5
-	OnActionCreated = function (self, entity, data, sender)
-		LogAlways("[%s] TOSSHARED.OnActionCreated sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
-	end,
-
+	-- События из кода CRYENGINE 5
+	-- Вызывается только OnActionDone
 	OnActionDone = function (self, entity, data, sender)
 		LogAlways("[%s] TOSSHARED.OnActionDone sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
 	end,
 
-	OnActionEnd = function (self, entity, data, sender)
-		LogAlways("[%s] TOSSHARED.OnActionEnd sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
+	-- OnActionCreated = function (self, entity, data, sender)
+	-- 	LogAlways("[%s] TOSSHARED.OnActionCreated sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
+	-- end,
+
+	-- OnActionEnd = function (self, entity, data, sender)
+	-- 	LogAlways("[%s] TOSSHARED.OnActionEnd sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
+	-- end,
+
+	-- OnActionStart = function (self, entity, data, sender)
+	-- 	LogAlways("[%s] TOSSHARED.OnActionStart sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
+	-- end,
+	---------------------------------------------
+	OnObjectSeen = function( self, entity, data )
+		-- called when the enemy can no longer see its foe, but remembers where it saw it last
+		LogAlways("[%s] TOSSHARED.OnObjectSeen data: %s", EntityName(entity), table.safedump(data))
 	end,
 
-	OnActionStart = function (self, entity, data, sender)
-		LogAlways("[%s] TOSSHARED.OnActionStart sender: %s, data: %s", EntityName(entity), EntityName(sender), EntityName(data))
+	---------------------------------------------
+	OnFriendInWay = function( self, entity, data )
+		-- called when the enemy can no longer see its foe, but remembers where it saw it last
+		LogAlways("[%s] TOSSHARED.OnFriendInWay data: %s", EntityName(entity), table.safedump(data))
 	end,
 
+	--------------------------------------------------------------------------
+	OnSeenByEnemy = function( self, entity, sender )
+		LogAlways("[%s] TOSSHARED.OnSeenByEnemy sender: %s", EntityName(entity), EntityName(sender))
+	end,
+	--------------------------------------------------------------------------
+	OnCloseContact= function( self, entity )
+		LogAlways("[%s] TOSSHARED.OnCloseContact sender: %s", EntityName(entity))
+	end,
+
+	OnTargetDead = function( self, entity )
+		-- called when the attention target died
+		LogAlways("[%s] TOSSHARED.OnTargetDead", EntityName(entity))
+	end,
+
+	OnTargetNavTypeChanged= function(self,entity,sender,data)
+		LogAlways("[%s] TOSSHARED.OnTargetNavTypeChanged sender: %s, data: %s", EntityName(entity), EntityName(sender), table.safedump(data))
+	end,
+
+	---------------------------------------------
+	OnTargetCloaked = function(self, entity)
+		LogAlways("[%s] TOSSHARED.OnTargetCloaked", EntityName(entity))
+	end,
+
+	---------------------------------------------
+	ORDER_TIMEOUT = function( self, entity, sender,data )
+		LogAlways("[%s] TOSSHARED.ORDER_TIMEOUT", EntityName(entity))
+	end,
+	
+	---------------------------------------------
+	ORDER_ACQUIRE_TARGET = function(self , entity, sender, data)
+		LogAlways("[%s] TOSSHARED.ORDER_ACQUIRE_TARGET sender: %s", EntityName(entity), EntityName(sender), table.safedump(data))
+	end,
+
+	---------------------------------------------------------------------------------------------------------------------------------------
+	ORDER_COVER_SEARCH = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.ORDER_COVER_SEARCH sender: %s", EntityName(entity), EntityName(sender))
+	end,	
+
+	---------------------------------------------------------------------------------------------------------------------------------------
+	ORD_ATTACK = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.ORD_ATTACK sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	ORD_DONE = function( self, entity, sender )
+		LogAlways("[%s] TOSSHARED.ORD_DONE sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	OnReinforcementRequested = function ( self, entity, sender, extraData )
+		LogAlways("[%s] TOSSHARED.OnReinforcementRequested sender: %s", EntityName(entity), EntityName(sender), table.safedump(extraData))
+	end,
+	--------------------------------------------------------------------------
+	OnCallReinforcement = function(self, entity, sender, extraData)
+		LogAlways("[%s] TOSSHARED.OnCallReinforcement sender: %s", EntityName(entity), EntityName(sender), table.safedump(extraData))
+	end,
+	--------------------------------------------------------------------------
+	OnPathFound = function( self, entity, sender )
+		LogAlways("[%s] TOSSHARED.OnPathFound sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	--------------------------------------------------------------
+	OnRequestUpdate = function( self, entity, sender )
+		LogAlways("[%s] TOSSHARED.OnRequestUpdate sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	OnPlayerDied = function( self, entity, sender )
+		LogAlways("[%s] TOSSHARED.OnPlayerDied sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	---------------------------------------------
+	OnLeaderDied = function ( self, entity, sender)
+		LogAlways("[%s] TOSSHARED.OnLeaderDied sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	---------------------------------------------
+	OnLeaderAssigned = function ( self, entity, sender)
+		-- Вызывается когда сущность entity назначается лидер группы через AI.SetLeader(entityId)
+		LogAlways("[%s] TOSSHARED.OnLeaderAssigned sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	---------------------------------------------
+	OnLeaderDeassigned = function ( self, entity, sender)
+		-- Вызывается когда сущность entity разназначается лидером группы
+		LogAlways("[%s] TOSSHARED.OnLeaderDeassigned sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	-----------------------------------------------------------
+	OnUnitDied = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.OnUnitDied sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	-----------------------------------------------------------
+	OnJoinTeam = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.OnJoinTeam sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	-----------------------------------------------------------
+	GroupMemberDied = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.GroupMemberDied sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	--------------------------------------------------
+	OnBodyFallSound = function(self, entity, sender, data)
+		LogAlways("[%s] TOSSHARED.OnBodyFallSound sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	OnGroupMemberDiedNearest = function ( self, entity, sender)
+		LogAlways("[%s] TOSSHARED.OnGroupMemberDiedNearest sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	-- OnDeath = function ( self, entity, sender)
+	-- 	LogAlways("[%s] TOSSHARED.OnDeath sender: %s", EntityName(entity), EntityName(sender))
+	-- end,
+
+	--------------------------------------------------
+	ENEMYSEEN_FIRST_CONTACT = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.ENEMYSEEN_FIRST_CONTACT sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	--------------------------------------------------
+	ENEMYSEEN_DURING_COMBAT = function(self,entity,sender)
+		LogAlways("[%s] TOSSHARED.ENEMYSEEN_DURING_COMBAT sender: %s", EntityName(entity), EntityName(sender))
+	end,
+
+	OnBackOffFailed = function(self,entity)
+		LogAlways("[%s] TOSSHARED.OnBackOffFailed sender: %s", EntityName(entity))
+	end,
+
+	---------------------------------------------
+	OnVehicleDanger = function(self,entity)
+		LogAlways("[%s] TOSSHARED.OnVehicleDanger sender: %s", EntityName(entity))
+	end,
+	
+	---------------------------------------------
+	OnExplosionDanger = function(self,entity,sender,data)
+		LogAlways("[%s] TOSSHARED.OnExplosionDanger sender: %s", EntityName(entity), EntityName(sender), table.safedump(data))
+	end,
+
+	--------------------------------------------------
+	OnGrenadeDanger = function( self, entity, sender, signalData )
+		LogAlways("[%s] TOSSHARED.OnGrenadeDanger sender: %s", EntityName(entity), EntityName(sender), table.safedump(signalData))
+	end,
 
 	-- SYSTEM EVENTS			-----
 	---------------------------------------------
@@ -162,16 +319,16 @@ AIBehaviour.TOSSHARED = {
 		LogAlways("[%s] TOSSHARED.OnCoverCompromised", EntityName(entity))
 	end,
 
-	-- ---------------------------------------------
-	-- OnFallAndPlayWakeUp = function( self, entity )
-	-- 	AI_Utils:CommonContinueAfterReaction(entity);
-	-- 	LogAlways("[%s] TOSSHARED.OnFallAndPlayWakeUp", EntityName(entity))
-	-- end,
+	---------------------------------------------
+	OnFallAndPlayWakeUp = function( self, entity )
+		AI_Utils:CommonContinueAfterReaction(entity);
+		LogAlways("[%s] TOSSHARED.OnFallAndPlayWakeUp", EntityName(entity))
+	end,
 
-	-- ---------------------------------------------
-	-- OnPlayerTeamKill = function(self,entity,sender,data)
-	-- 	LogAlways("[%s] TOSSHARED.OnPlayerTeamKill", EntityName(entity))
-	-- end,
+	---------------------------------------------
+	OnPlayerTeamKill = function(self,entity,sender,data)
+		LogAlways("[%s] TOSSHARED.OnPlayerTeamKill", EntityName(entity))
+	end,
 	--------------------------------------------------
 
 	-- GROUP SIGNALS
@@ -224,12 +381,3 @@ AIBehaviour.TOSSHARED = {
 		-- team leader instructs groups to initiate part one of assault fire maneuver
 	end,
 }
-
--- LogAlways("TOSSHARED = %s", tostring( AIBehaviour.TOSSHARED))
--- LogAlways("TOSSHARED.Constructor = %s", tostring( AIBehaviour.TOSSHARED.Constructor))
--- LogAlways("TOSSHARED.Destructor = %s", tostring( AIBehaviour.TOSSHARED.Destructor))
--- LogAlways("TOSSHARED.ENABLE_COMBAT = %s", tostring( AIBehaviour.TOSSHARED.ENABLE_COMBAT))
--- LogAlways("TOSSHARED.OnActionCreated = %s", tostring( AIBehaviour.TOSSHARED.OnActionCreated))
--- LogAlways("TOSSHARED.OnActionDone = %s", tostring( AIBehaviour.TOSSHARED.OnActionDone))
--- LogAlways("TOSSHARED.OnActionEnd = %s", tostring( AIBehaviour.TOSSHARED.OnActionEnd))
--- LogAlways("TOSSHARED.OnActionStart = %s", tostring( AIBehaviour.TOSSHARED.OnActionStart))
